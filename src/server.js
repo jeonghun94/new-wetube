746f8bfefd7f67e7edc25f7fd90f8a6e1195c039
조정훈 <jeongh1021@naver.com>
2023-02-10 22:07:02 +0900

    서버 시작 db 경고 메세지 fix

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import "./models/Video";
 
 const app = express();
 const logger = morgan("dev");
